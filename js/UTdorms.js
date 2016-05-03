c41cc833a25c52438f668005b16d7020b7b27db0
sc1f <tanjunyuan@hotmail.co.uk>
2016-05-02 21:44:01 -0500

    first commit

---
+++ js/UTdorms.js
@@ -0,0 +1,345 @@
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2NpZiIsImEiOiJjaWgyOHJkZW8weHJrd3dtMHJ1cnV0ZDY2In0.5wK8t52sB90Exoi6StYlBw';
+
+var geoData = [
+    {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Andrews",
+                    "address": "2401 Whitis Avenue",
+                    "link": "andrews"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73982524871825,
+                        30.288177779561856
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "blanton",
+                    "name": "Blanton",
+                    "address": "2500 University Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73937463760376,
+                        30.288661840447016
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Brackenridge",
+                    "link": "brackenridge",
+                    "address": "303 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73593068122864,
+                        30.283135529663728
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "carothers",
+                    "name": "Carothers",
+                    "address": "2501 Whitis Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.74010956287384,
+                        30.288689633344013
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "creekside",
+                    "name": "Creekside",
+                    "address": "2500 San Jacinto Blvd"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73337721824645,
+                        30.288527508000662
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "duren",
+                    "name": "Duren",
+                    "address": "2604 Whitis Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.74039924144745,
+                        30.291390138943623
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Jester East",
+                    "link": "jeast",
+                    "address": "201 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73595750331877,
+                        30.2823758113038
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Jester West",
+                    "link": "jwest",
+                    "address": "201 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73670315742491,
+                        30.282125658849186
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "kinsolving",
+                    "name": "Kinsolving",
+                    "address": "2605 Whitis Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73965895175934,
+                        30.290320136184143
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "littlefield",
+                    "name": "Littlefield",
+                    "address": "2503 Whitis Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73972332477568,
+                        30.289319603563253
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Moore-Hill",
+                    "link": "moorehill",
+                    "address": "304 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73538887500763,
+                        30.283603402068238
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Prather",
+                    "link": "prather",
+                    "address": "305 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73509919643402,
+                        30.282389708643695
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "Roberts",
+                    "link": "roberts",
+                    "address": "303 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73516893386841,
+                        30.2830660434728
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "name": "San Jacinto",
+                    "address": "309 East 21st Street"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.73434281349182,
+                        30.282496254850795
+                    ]
+                }
+            },
+            {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#fd9159",
+                    "marker-size": "medium",
+                    "marker-symbol": "building",
+                    "link": "whitis",
+                    "name": "Whitis Court",
+                    "address": "2610 Whitis Avenue"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -97.74073183536528,
+                        30.290852822999625
+                    ]
+                }
+            }
+        ]
+    }
+];
+
+var map = L.mapbox.map('homepage-map', 'mapbox.streets')
+    .setView([30.287, -97.738], 16);
+
+var listings = document.getElementById('listings');
+var locations = L.mapbox.featureLayer().addTo(map);
+
+locations.setGeoJSON(geoData);
+
+function setActive(el) {
+    var siblings = listings.getElementsByTagName('div');
+    for (var i = 0; i < siblings.length; i++) {
+        siblings[i].className = siblings[i].className
+            .replace(/active/, '').replace(/\s\s*$/, '');
+    }
+
+    el.className += ' active';
+}
+
+locations.eachLayer(function (locale) {
+
+    // Shorten locale.feature.properties to just `prop` so we're not
+    // writing this long form over and over again.
+    var prop = locale.feature.properties;
+
+    // Each marker on the map.
+    var popup = '<a href="' + prop.link + '" target="_blank">' + '<h2>' + prop.name + '</h2></a><p>' + prop.address + "</p>";
+
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'list-item';
+
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+
+    link.innerHTML = prop.name;
+
+    link.onclick = function () {
+        setActive(listing);
+
+        // When a menu item is clicked, animate the map to center
+        // its associated locale and open its popup.
+        map.setView(locale.getLatLng(), 18);
+        locale.openPopup();
+        return false;
+    };
+
+    var direct_link = listing.appendChild(document.createElement('a'));
+    direct_link.href = '/' + prop.link;
+    direct_link.target = '_blank';
+    direct_link.className = 'direct-link';
+    direct_link.innerHTML = "<i class='fa fa-external-link'></i>";
+
+    // Marker interaction
+    locale.on('click', function (e) {
+        // 1. center the map on the selected marker.
+        map.panTo(locale.getLatLng());
+
+        // 2. Set active the markers associated listing.
+        setActive(listing);
+    });
+
+    popup += '</div>';
+    locale.bindPopup(popup);
+});
+
+
